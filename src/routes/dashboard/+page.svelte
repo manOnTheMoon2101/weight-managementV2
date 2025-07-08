3e29b8e2b3152095531064ac764ca3c9def98c44
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2025-07-08 20:27:31 +0200

    ui updates

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -1,6 +1,13 @@
 <div class="flex flex-col gap-1">
 	Dashboard index page
 	<div class="rounded-md border bg-card p-1 font-mono text-card-foreground">
+		<Button href="/" class="w-fit" size="lg">Back to Home</Button>
+
 		routes/dashboard/+page.svelte
 	</div>
 </div>
+
+
+<script lang="ts">
+import { Button } from "$lib/components/ui/button";
+</script>
