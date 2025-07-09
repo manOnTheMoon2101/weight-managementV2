7e871a583e58c91ee6dcf13e4f003c5edd199a20
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2025-07-09 20:23:23 +0200

    fix

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -5,5 +5,5 @@ import { env } from "$env/dynamic/private";
 
 if (!env.BETTER_AUTH_URL) throw new Error("BETTER_AUTH_URL is not set");
 export const authClient = createAuthClient({
-	baseURL: env.DATABASE_URL || "https://weight-management-v2.vercel.app",
+	baseURL: env.BETTER_AUTH_URL || "https://weight-management-v2.vercel.app",
 });
