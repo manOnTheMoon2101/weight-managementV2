96a58558690e15ea428d7996bd6aaba6d889a85c
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2025-07-09 20:14:52 +0200

    idiot

--- src/lib/server/schema/index.ts
+++ src/lib/server/schema/index.ts
@@ -1,4 +1,5 @@
 export * from "./auth.schema";
 export * from "./supplements";
 export * from "./nutrients";
-export * from "./health_tracking"
+export * from "./health_tracking";
+export * from "./sleep_schedule"
