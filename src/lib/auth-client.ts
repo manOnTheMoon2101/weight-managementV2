b9faa6e45434de67e348157d37950af1d239a434
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2025-07-09 20:21:03 +0200

    url set

--- src/lib/auth-client.ts
+++ src/lib/auth-client.ts
@@ -1,6 +1,9 @@
 
 import { createAuthClient } from "better-auth/svelte";
+import { env } from "$env/dynamic/private";
 
+
+if (!env.BETTER_AUTH_URL) throw new Error("BETTER_AUTH_URL is not set");
 export const authClient = createAuthClient({
-	baseURL: "http://localhost:5173",
+	baseURL: env.DATABASE_URL || "https://weight-management-v2.vercel.app",
 });
