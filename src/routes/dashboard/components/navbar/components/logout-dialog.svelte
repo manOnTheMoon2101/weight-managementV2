8371c69807d49c66327fc689734dbfdc01b890ef
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2025-07-11 19:34:48 +0200

    dialog update

--- src/routes/dashboard/components/navbar/components/logout-dialog.svelte
+++ src/routes/dashboard/components/navbar/components/logout-dialog.svelte
@@ -1,20 +1,26 @@
 <script lang="ts">
     import * as AlertDialog from "$lib/components/ui/alert-dialog/index.js";
+    import { Button } from "$lib/components/ui/button";
+   import X from "@lucide/svelte/icons/x"
    </script>
     
    <AlertDialog.Root>
     <AlertDialog.Trigger>Logout</AlertDialog.Trigger>
     <AlertDialog.Content>
      <AlertDialog.Header>
-      <AlertDialog.Title>Are you absolutely sure?</AlertDialog.Title>
-      <AlertDialog.Description>
+      <div class="flex flex-row justify-between items-center">
+        <AlertDialog.Title>Are you absolutely sure?</AlertDialog.Title>
+      <AlertDialog.Cancel class="text-red-800 hover:bg-red-800/40"><X /></AlertDialog.Cancel>
+      </div>
+      <!-- <AlertDialog.Description>
        This action cannot be undone. This will permanently delete your account
        and remove your data from our servers.
-      </AlertDialog.Description>
+      </AlertDialog.Description> -->
      </AlertDialog.Header>
-     <AlertDialog.Footer>
-      <AlertDialog.Cancel>Cancel</AlertDialog.Cancel>
-      <AlertDialog.Action>Continue</AlertDialog.Action>
-     </AlertDialog.Footer>
+     <div class="flex flex-row justify-center">
+        <AlertDialog.Footer>
+            <Button href="/" class="w-fit" size="lg">Logout</Button>
+           </AlertDialog.Footer>
+     </div>
     </AlertDialog.Content>
    </AlertDialog.Root>
