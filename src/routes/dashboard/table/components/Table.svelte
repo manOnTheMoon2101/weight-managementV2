6a63af8cc51681a590d237784dca194a152e40f6
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2025-07-29 20:15:10 +0200

    cleanup

--- src/routes/dashboard/table/components/Table.svelte
+++ src/routes/dashboard/table/components/Table.svelte
@@ -129,15 +129,6 @@
 			gridApi.setGridOption("quickFilterText", filterValue);
 		}
 	}
-
-	function formatDMY(dateString: string): string {
-		const date = new Date(dateString);
-		const day = date.getDate().toString().padStart(2, "0");
-		const month = (date.getMonth() + 1).toString().padStart(2, "0");
-		const year = date.getFullYear();
-		return `${year}-${month}-${day}`;
-	}
-
 	onMount(() => {
 		const gridOptions: GridOptions<any> = {
 			theme: darkTheme,
