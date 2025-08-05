eaf83f2e2aba8a88012fb4d10aa0268e872345d8
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2025-08-05 21:28:04 +0200

    analytics

--- src/routes/dashboard/+layout.svelte
+++ src/routes/dashboard/+layout.svelte
@@ -2,10 +2,15 @@
 	import type { Snippet } from "svelte";
 	import type { LayoutData } from "./$types";
 	import Navbar from "./components/navbar/Navbar.svelte";
+	import { injectAnalytics } from '@vercel/analytics/sveltekit';
 	let { data, children }: { data: any; children: Snippet } = $props();
+	import { dev } from '$app/environment';
+
+injectAnalytics({ mode: dev ? 'development' : 'production' });
 </script>
 
 <div class="flex flex-col gap-4 p-4">
 	<Navbar user={data.user} limits={data.limits} userColour={data.userColour.colour} />
+	
 	{@render children()}
 </div>
