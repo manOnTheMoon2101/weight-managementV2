0e180c9c805a43b026bcf750a5a8775cc74ec415
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2025-08-10 16:13:10 +0200

    work

---
+++ .drizzle/0018_glorious_the_stranger.sql
@@ -0,0 +1,2 @@
+ALTER TABLE "supplements" ADD COLUMN "userId" text NOT NULL;--> statement-breakpoint
+ALTER TABLE "supplements" ADD CONSTRAINT "supplements_userId_user_id_fk" FOREIGN KEY ("userId") REFERENCES "public"."user"("id") ON DELETE cascade ON UPDATE no action;
