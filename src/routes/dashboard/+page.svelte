b72c5b9345741ab58dfa378ce2242d6f9556c018
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2025-08-10 15:40:29 +0200

    nice

--- src/routes/dashboard/+page.svelte
+++ src/routes/dashboard/+page.svelte
@@ -4,12 +4,13 @@
 	import ChevronUp from "@lucide/svelte/icons/chevron-up";
 	import * as Tooltip from "$lib/components/ui/tooltip/index.js";
 	import FootPrint from "@lucide/svelte/icons/footprints";
-	import { DateFormatter} from "@internationalized/date";
-import Weight from "./components/charts/Weight.svelte";
+	import Droplet from "@lucide/svelte/icons/droplet";
+	import { DateFormatter } from "@internationalized/date";
+	import Weight from "./components/charts/Weight.svelte";
 	let { data }: { data: PageData } = $props();
 
 	let user = $derived(data.user);
-	let weightCharts = $derived(data.weightCharts)
+	let weightCharts = $derived(data.weightCharts);
 	let currentWeight = $derived(data.currentWeight?.weight);
 	let currentWeightDate = $derived(data.currentWeight?.createdAt);
 	let previousWeight = $derived(data.previousWeight?.weight);
@@ -98,17 +99,21 @@ import Weight from "./components/charts/Weight.svelte";
 			</div>
 		</div>
 
-		<div>
-			Average Water Intake(ml) {Math.round(Number(averageWaterIntake))}
-			
-		</div>
-
 		<div class="flex flex-row items-center justify-evenly">
-			<Weight dateSeriesData={weightCharts}/>
-			<div class="flex flex-col justify-center items-center">
-				<span class="text-8xl flex flex-row items-center">{Math.round(Number(averageStepsIntake))} <FootPrint/></span>
+			<Weight dateSeriesData={weightCharts} />
+			<div class="flex flex-col items-center justify-center">
+				<span class="flex flex-row items-center text-8xl"
+					>{Math.round(Number(averageStepsIntake))} <FootPrint /></span
+				>
 
-				<h3 class="text-muted">Average Steps </h3>
+				<h3 class="text-muted">Average Steps</h3>
+			</div>
+			<div class="flex flex-col items-center justify-center">
+				<span class="flex flex-row items-center text-8xl"
+					>{Math.round(Number(averageWaterIntake))} <Droplet class="text-blue-400" /></span
+				>
+
+				<h3 class="text-muted">Average Water</h3>
 			</div>
 		</div>
 	</div>
