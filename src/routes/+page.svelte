54ca9a8ff7cce1665f74b06f228743bfb8351ac1
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2025-09-02 20:24:32 +0200

    redirect test

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,16 @@
 <script lang="ts">
 	import { authClient } from "$lib/auth-client";
 	import { Button } from "$lib/components/ui/button";
+	import { goto } from "$app/navigation";
+	import { onMount } from "svelte";
 
 	const session = authClient.useSession();
+
+	onMount(() => {
+		if ($session.data) {
+			goto('/dashboard');
+		}
+	});
 </script>
 
 <div class="flex flex-col gap-4 p-6">
