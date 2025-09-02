5389261d6042a43fa903be6fcea929b1ecebe5a1
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2025-09-02 20:33:14 +0200

    sheet close button hide

--- src/lib/components/ui/sheet/sheet-content.svelte
+++ src/lib/components/ui/sheet/sheet-content.svelte
@@ -43,11 +43,11 @@
 	<SheetOverlay />
 	<SheetPrimitive.Content bind:ref class={cn(sheetVariants({ side }), className)} {...restProps}>
 		{@render children?.()}
-		<SheetPrimitive.Close
+		<!-- <SheetPrimitive.Close
 			class="ring-offset-background focus:ring-ring data-[state=open]:bg-secondary absolute right-4 top-4 rounded-sm opacity-70 transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-offset-2 disabled:pointer-events-none"
 		>
 			<X class="size-4" />
 			<span class="sr-only">Close</span>
-		</SheetPrimitive.Close>
+		</SheetPrimitive.Close> -->
 	</SheetPrimitive.Content>
 </SheetPrimitive.Portal>
