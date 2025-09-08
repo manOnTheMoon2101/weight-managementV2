70bb93c2bbb14d7f043648583e58853dfd7d082e
Cleve Clayton <95036334+manOnTheMoon2101@users.noreply.github.com>
2025-09-08 20:41:35 +0200

    theme changer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,11 +4,20 @@
 	import { ModeWatcher } from "mode-watcher";
 	import { dev } from "$app/environment";
 	import { injectAnalytics } from '@vercel/analytics/sveltekit';
+	import { createInitialModeExpression } from "mode-watcher";
 	injectAnalytics({ mode: dev ? "development" : "production" });
 </script>
 
 <svelte:head>
 	<title>Weight Tracker</title>
+	<script>
+		{@html createInitialModeExpression({
+			defaultMode: "light",
+			defaultTheme: "",
+			modeStorageKey: "app-mode",
+			themeStorageKey: "app-theme",
+		})}
+	</script>
 </svelte:head>
 
 <ModeWatcher />
