76058f5b218996e7719478b245c214a56ffd81c6
manOnTheMoon2101 <clevejohnclayton.2101@gmail.com>
2025-11-11 20:54:39 +0200

    lets see

--- src/routes/dashboard/components/navbar/components/limits-dialog.svelte
+++ src/routes/dashboard/components/navbar/components/limits-dialog.svelte
@@ -3,10 +3,11 @@
 	import { Input } from "$lib/components/ui/input/index.js";
 	import { Label } from "$lib/components/ui/label/index.js";
 	import Button from "$lib/components/ui/button/button.svelte";
-
+	import { Switch } from "$lib/components/ui/switch/index.js";
 	let { limits } = $props<{ limits: any }>();
 
 	let updateLoading = $state(false);
+	let goalEnabled = $state(false);
 
 	function handleUpdateSubmit() {
 		updateLoading = true;
@@ -20,50 +21,75 @@
 			<Dialog.Title>Edit Limits</Dialog.Title>
 			<Dialog.Description>
 				<form method="POST" action="/dashboard?/updateLimits" onsubmit={handleUpdateSubmit}>
-	<div class="flex flex-row justify-between p-1 rounded-lg">
-				<div>
-						<Label for="caloriesLimit">Calories</Label>
-					<Input
-						name="caloriesLimit"
-						placeholder="Calories"
-						type={"number"}
-						value={limits?.caloriesLimit}
-					/>
+					<div class="flex flex-row justify-between rounded-lg p-1">
+						<div>
+							<Label for="caloriesLimit">Calories</Label>
+							<Input
+								name="caloriesLimit"
+								placeholder="Calories"
+								type={"number"}
+								value={limits?.caloriesLimit}
+							/>
 
-					<Label for="waterLimit">Water</Label>
-					<Input name="waterLimit" placeholder="Water" type={"number"} value={limits?.waterLimit} />
+							<Label for="waterLimit">Water</Label>
+							<Input
+								name="waterLimit"
+								placeholder="Water"
+								type={"number"}
+								value={limits?.waterLimit}
+							/>
 
-					<Label for="stepsLimit">Steps</Label>
-					<Input name="stepsLimit" placeholder="Steps" type={"number"} value={limits?.stepsLimit} />
-			</div>
+							<Label for="stepsLimit">Steps</Label>
+							<Input
+								name="stepsLimit"
+								placeholder="Steps"
+								type={"number"}
+								value={limits?.stepsLimit}
+							/>
+							<div class="my-2">
+								<Label for="journey">{goalEnabled ? "Weight Loss" : "Bulk"}</Label>
 
-<div>
+								<Switch name="journey" bind:checked={goalEnabled} />
+							</div>
+						</div>
 
-					<Label for="fatLimit">Fat</Label>
-					<Input name="fatLimit" placeholder="Fat" type={"number"} value={limits?.fatLimit} />
+						<div>
+							<Label for="fatLimit">Fat</Label>
+							<Input name="fatLimit" placeholder="Fat" type={"number"} value={limits?.fatLimit} />
 
-					<Label for="carbsLimit">Carbs</Label>
-					<Input name="carbsLimit" placeholder="Carbs" type={"number"} value={limits?.carbsLimit} />
+							<Label for="carbsLimit">Carbs</Label>
+							<Input
+								name="carbsLimit"
+								placeholder="Carbs"
+								type={"number"}
+								value={limits?.carbsLimit}
+							/>
 
-					<Label for="proteinLimit">Protein</Label>
-					<Input
-						name="proteinLimit"
-						placeholder="Protein"
-						type={"number"}
-						value={limits?.proteinLimit}
-					/>
+							<Label for="proteinLimit">Protein</Label>
+							<Input
+								name="proteinLimit"
+								placeholder="Protein"
+								type={"number"}
+								value={limits?.proteinLimit}
+							/>
 
-					<Label for="sugarLimit">Sugar</Label>
-					<Input name="sugarLimit" placeholder="Sugar" type={"number"} value={limits?.sugarLimit} />
-</div>
-	</div>
-					<div class="flex flex-row justify-end mt-4">
+							<Label for="sugarLimit">Sugar</Label>
+							<Input
+								name="sugarLimit"
+								placeholder="Sugar"
+								type={"number"}
+								value={limits?.sugarLimit}
+							/>
+						</div>
+					</div>
+
+					<div class="mt-4 flex flex-row justify-end">
 						{#if !updateLoading}
-							<Button type="submit" variant='save'>Save</Button>
+							<Button type="submit" variant="save">Save</Button>
 						{:else}
-							<Button type="submit" variant='save' disabled>
+							<Button type="submit" variant="save" disabled>
 								<div class="flex items-center justify-center space-x-2">
-									<div class="h-4 w-4 animate-spin rounded-full border-b-2 border-accent"></div>
+									<div class="border-accent h-4 w-4 animate-spin rounded-full border-b-2"></div>
 									<span>Saving...</span>
 								</div>
 							</Button>
