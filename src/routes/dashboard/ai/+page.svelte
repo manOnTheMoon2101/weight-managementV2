821e3f2a8940002ea7f05d85b441a2c213a54f35
manOnTheMoon2101 <clevejohnclayton.2101@gmail.com>
2025-12-01 21:04:04 +0200

    warning ai message

--- src/routes/dashboard/ai/+page.svelte
+++ src/routes/dashboard/ai/+page.svelte
@@ -1,9 +1,25 @@
 <script lang="ts">
+	import { Button } from "$lib/components/ui/button/index.js";
+	import { Textarea } from "$lib/components/ui/textarea/index.js";
+    import {Badge} from "$lib/components/ui/badge/index"
 
+	let warning = $state("");
 
+	function setPrank() {
+		warning = "AI Coming Soon!!!";
+	}
+
+    // function clearPrank(){
+    //     warning = ""
+    // }
 </script>
 
-
-<div>
-    AI
+<div class="grid w-full gap-2">
+	<Textarea placeholder="Enter Foods Here." />
+	<Button class="bg-secondary" onclick={setPrank}>Generate</Button>
 </div>
+
+{#if warning}
+<!-- <Button class="bg-secondary" onclick={clearPrank}>Clear</Button> -->
+	<div class="text-center"><Badge class="bg-destructive text-2xl text-foreground">{warning}</Badge></div>
+{/if}
