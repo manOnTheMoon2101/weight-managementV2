b9ba3ef4df3643eeb83808317265f58884ac6266
manOnTheMoon2101 <clevejohnclayton.2101@gmail.com>
2026-01-03 16:09:40 +0200

    types

--- src/routes/dashboard/table/components/Table.svelte
+++ src/routes/dashboard/table/components/Table.svelte
@@ -31,14 +31,13 @@
 		headerBackgroundColor: `var(--secondary)`,
 		headerTextColor: "white",
 		oddRowBackgroundColor: "var(--primary)",
-		rowHoverColor:'var(--secondary)',
+		rowHoverColor: "var(--secondary)",
 		headerFontSize: 14,
 		fontSize: 13,
 	});
 
 	let gridDiv: HTMLDivElement;
 	let filterValue = $state("");
-
 
 	function onFilterTextBoxChanged(
 		event: Event & { currentTarget: EventTarget & HTMLInputElement }
@@ -53,14 +52,14 @@
 	function exportToCsv() {
 		if (gridApi) {
 			gridApi.exportDataAsCsv({
-				fileName: `Weight-Tracker-${new Date().toISOString().split('T')[0]}.csv`,
+				fileName: `Weight-Tracker-${new Date().toISOString().split("T")[0]}.csv`,
 			});
 		}
 	}
 	export { exportToCsv };
 	onMount(() => {
 		const isMobile = window.innerWidth < 768;
-		
+
 		const gridOptions: GridOptions<any> = {
 			theme: darkTheme,
 			columnDefs,
@@ -90,17 +89,15 @@
 			}
 		};
 
-		window.addEventListener('resize', handleResize);
-		return () => window.removeEventListener('resize', handleResize);
+		window.addEventListener("resize", handleResize);
+		return () => window.removeEventListener("resize", handleResize);
 	});
-
-
-
-
-
-
 </script>
 
-<div class="flex flex-col gap-4 shadow-lg shadow-secondary">
-	<div bind:this={gridDiv} class="ag-theme-quartz-dark w-full overflow-x-auto" style="height: 80vh;"></div>
+<div class="shadow-secondary flex flex-col gap-4 shadow-lg">
+	<div
+		bind:this={gridDiv}
+		class="ag-theme-quartz-dark w-full overflow-x-auto"
+		style="height: 80vh;"
+	></div>
 </div>
